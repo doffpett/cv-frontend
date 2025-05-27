b7307e86b2a248602a9f6ae104a965fda5d8d0a3
Christoffer Pettersen <christoffer@avoconsulting.no>
2025-05-27 13:54:08 +0200

    Fix: Bruker fersk data fra Sanity for å vise bilde

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -4,5 +4,5 @@ export const sanity = createClient({
   projectId: 'dftazm12', // ← bytt ut med ditt faktiske prosjekt-ID
   dataset: 'production',
   apiVersion: '2024-01-01',
-  useCdn: true
+  useCdn: false
 })
